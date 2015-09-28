0e734391f9d41ca742319f40e4e4be7956ab9118
Ed Liddle <ed_liddle@yahoo.co.nz>
2015-09-28 15:47:45 +1300

    check in code

---
+++ class_lib.php
@@ -0,0 +1,21 @@
+<?php
+
+     class person {
+           // properties
+           var $name;
+           
+           // methods
+           function set_name($new_name) {
+                    $this->name = $new_name;
+           }
+           
+           function get_name() {
+                    return $this->name;
+           }
+
+
+     }
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<body>
+<?php
+
+include("class_lib.php");
+
+//instantiate = create an instance of an object in memory
+
+$stefan = new person();
+$jimmy = new person;
+$stefan->set_name("Stefan Mischook");
+$jimmy->set_name("Nick Waddles");
+
+echo "Stefan's full name: " . $stefan->get_name();
+
+echo "Nick's full name: " . $jimmy->get_name();
+
+?>
+
+</body>
